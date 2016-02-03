0f80aaaf9d2235e340eb946c8f44b024064878ae
Adam <adamtibballs@gmail.com>
2016-02-03 21:34:19 +1000

    Update talks page to work of json data

---
+++ scripts/audio.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  $.getJSON('data/audio.json')
+  .done(function( data ){
+
+    var html = data.audio.reverse().map(function(audioGroup){
+      var talksHtml = audioGroup.talks.map(function(talk){
+        var name  = "<h3>"+talk.name+"</h3>";
+        var audio = "<div class='row'><audio src='{source}' preload='none'/></div>";
+        var source = "..\\audio\\{year}\\{fileName}"
+                      .replace("{year}", audioGroup.year)
+                      .replace("{fileName}", talk.fileName);
+
+        audio = audio.replace("{source}", source);
+
+        return name + audio;
+      });
+      return "<h2>"+audioGroup.year+"</h2>" + talksHtml.join("");
+    });
+
+
+    $("#content").html(html.join(""));
+
+    audiojs.events.ready(function() {
+      var as = audiojs.createAll();
+    });
+
+
+  })
+  .fail(function(error) {
+    console.log( 'error', error );
+  });
+
+});
